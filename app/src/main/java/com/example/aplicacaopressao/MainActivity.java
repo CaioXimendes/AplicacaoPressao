47371c41c8531da5b2b1751d6d66bc884dfc3657
CaioXimendes <caioximendes1@gmail.com>
2024-11-27 19:48:29 -0300

    getting an input text from the field 1

--- app/src/main/java/com/example/aplicacaopressao/MainActivity.java
+++ app/src/main/java/com/example/aplicacaopressao/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.aplicacaopressao;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
+    TextView textView;
+    Button buttonTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editTextNumber);
+        buttonTest = (Button) findViewById(R.id.button);
+
+        buttonTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(editText.getText().toString());
+                //editText.setText(String.valueOf(999));
+                //Log.v("EditText",editText.getText().toString());
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
     }
 }
